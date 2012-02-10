194949fe699c0cbdc3b598646ccbbb1e2010cc3f
Hugo Caille <hugo@caille.tk>
2012-02-10 19:36:03 +0100

    More verbose digger.py and support for ignored.json

--- digger.py
+++ digger.py
@@ -1,43 +1,53 @@
-import json
-import time
+import json, time
 
 source = raw_input('Source Userame: ')
 target = raw_input('Target Username: ')
+
+def importmsg(data):
+    print str(len(data)) + ' scrobbles have been imported.'
+
+try:
+    f = open('ignored.json','r')
+    print 'Importing ignored.json file...'
+    data = json.loads(f.read())
+    f.close()
+    importmsg(data)
+except:
+    data = list()
+
+print 'Parsing ' + target + '.json file...'
 f = open(target+'.json', 'r')
-data = json.loads(f.read())
+target_data = json.loads(f.read())
 f.close()
+importmsg(target_data)
+for track in target_data:
+    data.append(track)
 
 holes = list()
-
-available = 0
+print 'Searching for running time space...'
 for track in data:
     if track['Duration'] != None:
         result = {'Begin' : track['Time'] + track['Duration'], 'End' : int(time.time()) }
-        available = available + (result['End'] - result['Begin'])
         holes.append(result)
         break
 
+print 'Searching for spaces...'
 i = 0
 while i <= len(data) - 2 :
     if data[i + 1]['Duration'] != None:
         if (data[i]['Time'] - (data[i+1]['Duration'] + data[i+1]['Time']))  > 3600*2 and (time.time() - data[i]['Time']) < 3600*24*15:
             dic = {'Begin': data[i+1]['Time'] + data[i+1]['Duration'],  'End' : data[i]['Time']}
             holes.append(dic)
-            available = available + (dic['End'] - dic['Begin'])
     i = i +1
 
 scrobbles = list()
+print 'Parsing ' + source + '.json file...'
 f = open(source+'.json', 'r')
 data = json.loads(f.read())
+importmsg(data)
 f.close()
 
-goal = 0
-for track in data:
-    if track['Duration'] != None:
-        goal = goal + track['Duration']
-
-if available - goal < 0:
-    print "WARNING : All scrobbles can't be proceed cause of a lack of space on the new account."
+print 'Filling spaces...'
 for hole in holes:
     i = hole['Begin']
     for track in data:
@@ -46,7 +56,16 @@ for hole in holes:
                 scrobbles.append( {'Artist' : track['Artist'], 'Name' : track['Name'], 'Time' : i} )
                 i = i + track['Duration']
                 data.remove(track)
+        else:
+            data.remove(track)
 
+print 'Writing scrobbles.json file...'
 f = open('scrobbles.json', 'w')
 f.write(json.dumps(scrobbles))
 f.close()
+
+if len(data) > 0:
+    f = open('ignored.json', 'w')
+    f.write(json.dumps(data))
+    f.close()
+    print 'NOTICE : All scrobbles can\'t be imported cause of a lack of spaces on the new account.\nIgnored scrobbles have been saved to ignored.json . They will be added to your scrobbles next time you run digger.py'
